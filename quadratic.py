7192d554f11ce021aaa959b22581c7caebe048fc
Sowmika Rachakonda <sowmikarachakonda5@gmail.com>
2026-05-31 16:13:13 +0530

    Delete quadratic.py

--- quadratic.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Enter the length of a"))
-b = int(input("Enter the length of b"))
-c = int(input("Enter the length of c"))
-x1 = -b + (b**2-(4*a*c))**0.5/2*a
-x2 = -b - (b**2-(4*a*c))**0.5/2*a
-print(x1)
-print(x2)
