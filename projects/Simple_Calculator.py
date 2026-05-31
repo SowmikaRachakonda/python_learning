245de489d6aea273214ebd518b3edd2ab8b5b085
Sowmika Rachakonda <sowmikarachakonda5@gmail.com>
2026-05-31 16:51:40 +0530

    Create Simple_Calculator.py

---
+++ projects/Simple_Calculator.py
@@ -0,0 +1,30 @@
+a = float(input("Enter the value of a: "))
+
+b = float(input("Enter the value of b: "))
+
+operation = input("Enter the operation: ")
+
+if operation == "+":
+    print(a+b)
+  
+elif operation == "-":
+    print(a-b)
+  
+elif operation == "*":
+    print(a*b)
+  
+elif operation == "/":
+  
+    if b==0:
+        print("Error: Cannot divide by zero!")
+    else: 
+        print(a/b)
+      
+elif operation == "//":
+    print(a//b)
+  
+elif operation == "%":
+    print(a%b)
+  
+else:
+    print("Invalid Operation")
